0bf48928b7e1679efc1e00eed6bc8e388d7165dd
Sebastian Liu <sebsadface@gmail.com>
2023-04-17 02:14:00 -0700

    lab3 ready for test

--- cse369/lab3/lab3_files/lab3.sv
+++ cse369/lab3/lab3_files/lab3.sv
@@ -1,1 +1,17 @@
-sd
+module lab3 (
+    input logic [3:0] SW,
+    output logic [1:0] LEDR
+);
+
+    logic U, P, C, MARK, DISCOUNTED, STOLEN;
+
+    assign U = SW[0];
+    assign P = SW[1];
+    assign C = SW[2];
+    assign MARK = SW[3];
+    assign DISCOUNTED = P | (C & U);
+    assign STOLEN = ~(U | M | C) | (U & ~(P | M));
+    assign LEDR[0] = DISCOUNTED;
+    assign LEDR[1] = STOLEN;
+
+endmodule
--- cse369/lab3/lab3_files/lab3_tb.sv
+++ cse369/lab3/lab3_files/lab3_tb.sv
@@ -1,1 +1,12 @@
-fe
+module lab3_tb();
+    logic [3:0] SW,
+    logic [1:0] LEDR
+
+    integer i;
+    initial begin
+        for(i = 0; i < 16; i++) begin
+            SW[3:0] = i; #10;
+        end
+    end
+
+endmodule
