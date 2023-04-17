888fd9fcc0d4de3bd37501e4445cfd286bfa14de
Sebastian Liu <sebsadface@gmail.com>
2023-04-17 15:29:15 -0700

    lab3 ready for test

--- cse369/lab3/lab3_files/lab3.sv
+++ cse369/lab3/lab3_files/lab3.sv
@@ -1,17 +1,17 @@
 module lab3 (
-    input logic [3:0] SW,
-    output logic [1:0] LEDR
+    input logic [9:0] SW,
+    output logic [9:0] LEDR
     );
 
     logic U, P, C, M, DISCOUNTED, STOLEN;
 
-    assign U = SW[0];
-    assign P = SW[1];
-    assign C = SW[2];
-    assign M = SW[3];
+    assign U = SW[3];
+    assign P = SW[2];
+    assign C = SW[1];
+    assign M = SW[0];
     assign DISCOUNTED = P | (C & U);
     assign STOLEN = ~(U | M | C) | (U & ~(P | M));
-    assign LEDR[0] = DISCOUNTED;
-    assign LEDR[1] = STOLEN;
+    assign LEDR[1] = DISCOUNTED;
+    assign LEDR[0] = STOLEN;
 
 endmodule
