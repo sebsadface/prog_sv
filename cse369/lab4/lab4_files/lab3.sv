ea8803d3b681d78023371f6da9b9b527f553587a
Sebastian Liu <sebsadface@gmail.com>
2023-04-24 15:12:58 -0700

    ready for test

--- cse369/lab4/lab4_files/lab3.sv
+++ cse369/lab4/lab4_files/lab3.sv
@@ -1,7 +1,6 @@
-module lab4 (
+module lab3 (
     input logic [9:0] SW,
-    output logic [9:0] LEDR,
-    output logic [6:0] HEX0, HEX1, HEX3, HEX4, HEX5
+    output logic [9:0] LEDR
     );
 
     logic U, P, C, M, DISCOUNTED, STOLEN;
@@ -14,6 +13,5 @@ module lab4 (
     assign STOLEN = ~(U | M | C) | (U & ~(P | M));
     assign LEDR[1] = DISCOUNTED;
     assign LEDR[0] = STOLEN;
-    pawn SAFE (.upc(SW[3:1]), .leds0(HEX0), .leds1(HEX1), .leds2(HEX2), .leds3(HEX3), .leds4(HEX4), .leds5(HEX5));
 
 endmodule
---
+++ cse369/lab4/lab4_files/pawn.sv
@@ -0,0 +1,54 @@
+module pawn (
+  input  logic [2:0] upc,
+  output logic [6:0] leds0, leds1, leds2, leds3, leds4, leds5,
+  );
+
+  always_comb begin
+    case (upc)
+      //            Light: 6543210
+      3'b000: leds4 = 7'b0010010; // S
+      3'b000: leds3 = 7'b1001000; // A
+      3'b000: leds2 = 7'b0001110; // F
+      3'b000: leds1 = 7'b0000110; // E
+
+      3'b001: leds5 = 7'b1000110; // C
+      3'b001: leds4 = 7'b1000000; // O
+      3'b001: leds3 = 7'b0001110; // F
+      3'b001: leds2 = 7'b0001110; // F
+      3'b001: leds1 = 7'b0000110; // E
+      3'b001: leds0 = 7'b0000110; // E
+
+      3'b011: leds5 = 7'b1000110; // C
+      3'b011: leds4 = 7'b0001001; // H
+      3'b011: leds3 = 7'b0000110; // E
+      3'b011: leds2 = 7'b0010010; // S
+      3'b011: leds1 = 7'b0010010; // S
+
+      3'b100: leds5 = 7'b1001000; // A
+      3'b100: leds4 = 7'b0001100; // P
+      3'b100: leds3 = 7'b0001100; // P
+      3'b100: leds2 = 7'b1000111; // L
+      3'b100: leds1 = 7'b0000110; // E
+
+      3'b101: leds4 = 7'b1000110; // C
+      3'b101: leds3 = 7'b1000000; // O
+      3'b101: leds2 = 7'b0001100; // P
+      3'b101: leds1 = 7'b0110001; // Y
+
+      3'b110: leds5 = 7'b1000110; // C
+      3'b110: leds4 = 7'b0001001; // H
+      3'b110: leds3 = 7'b0000110; // E
+      3'b110: leds2 = 7'b0000110; // E
+      3'b110: leds1 = 7'b0010010; // S
+      3'b110: leds0 = 7'b0000110; // E
+
+
+      default: leds0 = 7'bX;
+      default: leds1 = 7'bX;
+      default: leds2 = 7'bX;
+      default: leds3 = 7'bX;
+      default: leds4 = 7'bX;
+      default: leds5 = 7'bX;
+    endcase
+  end
+endmodule  // seg7
