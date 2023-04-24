f98f2976ff4160ae9a3259f349eed7237471c51b
Sebastian Liu <sebsadface@gmail.com>
2023-04-24 00:01:45 -0700

    add hex0 hex1

--- cse369/lab4/lab4_files/lab4.sv
+++ cse369/lab4/lab4_files/lab4.sv
@@ -3,8 +3,8 @@ module lab4 (
     input logic [9:0] SW
 );
 
-seg7 H0 (.[3:0]bcd(~[3:0]SW), .[6:0]leds([6:0]HEX0));
+seg7 H0 (.bcd(~[3:0]SW), .leds([6:0]HEX0));
 
-seg7 H1 (.[3:0]bcd(~[4:7]SW), .[6:0]leds([6:0]HEX1));
+seg7 H1 (.bcd(~[4:7]SW), .leds([6:0]HEX1));
     
 endmodule
