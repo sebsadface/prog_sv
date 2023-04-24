0deab85c6eac6b8dacc3476f6c9a78fd529d1170
Sebastian Liu <sebsadface@gmail.com>
2023-04-24 00:33:34 -0700

    add hex0 hex1

--- cse369/lab4/lab4_files/lab4.sv
+++ cse369/lab4/lab4_files/lab4.sv
@@ -3,15 +3,7 @@ module lab4 (
     input logic [7:0] SW
 );
 
-logic [3:0] SW0, SW1;
-logic [6:0] HX0, HX1;
-
-assign SW0 = SW[3:0];
-assign SW1 = SW[7:4];
-not NHX0 (HEX0, HX0);
-not NHX1 (HEX1, HX1);  
-
-seg7 H0 (.bcd(SW0), .leds(HX0));
-seg7 H1 (.bcd(SW1), .leds(HX1)); 
-
+seg7 H0 (.bcd(SW[3:0]), .leds(HEX0));
+seg7 H1 (.bcd(SW[7:4]), .leds(HEX1));
+    
 endmodule
