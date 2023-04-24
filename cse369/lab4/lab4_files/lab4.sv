f4ea590f391e9501d04422731ab2231a7e454caa
Sebastian Liu <sebsadface@gmail.com>
2023-04-24 00:13:21 -0700

    add hex0 hex1

--- cse369/lab4/lab4_files/lab4.sv
+++ cse369/lab4/lab4_files/lab4.sv
@@ -1,12 +1,12 @@
 module lab4 (
-    output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
-    input logic [9:0] SW
+    output logic [6:0] HEX0, HEX1,
+    input logic [7:0] SW
 );
 
 logic [3:0] SW0, SW1;
 
-assign SW0 =  ~SW[3:0];
-assign SW1 = ~SW[7:4];
+assign SW0 = SW[3:0];
+assign SW1 = SW[7:4];
 
 seg7 H0 (.bcd(SW0), .leds(HEX0));
 
