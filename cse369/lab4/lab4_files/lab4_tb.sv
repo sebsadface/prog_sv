4339d298a1ecd5b755360994a92633f58e997573
Sebastian Liu <sebsadface@gmail.com>
2023-04-24 19:12:57 -0700

    ready for test

--- cse369/lab4/lab4_files/lab4_tb.sv
+++ cse369/lab4/lab4_files/lab4_tb.sv
@@ -1,7 +1,7 @@
 module lab4_tb();
     logic [9:0] SW;
     logic [9:0] LEDR;
-    logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
+    logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 
     lab4 dut (.LEDR, .SW, .HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5);
 
