f343c59312644a4f708a9925026c6698c402081b
Sebastian Liu <sebsadface@gmail.com>
2023-04-24 18:45:12 -0700

    ready for test

--- cse369/lab4/lab4_files/pawn.sv
+++ cse369/lab4/lab4_files/pawn.sv
@@ -6,14 +6,14 @@ module pawn (
   always_comb begin
     case (upc)
       //            Light: 6543210
-      3'b000: begin
+      begin 3'b000: 
         leds4 = 7'b0010010; // S
         leds3 = 7'b1001000; // A
         leds2 = 7'b0001110; // F
         leds1 = 7'b0000110; // E
       end
 
-      3'b001: begin
+      begin 3'b001:
         leds5 = 7'b1000110; // C
         leds4 = 7'b1000000; // O
         leds3 = 7'b0001110; // F
@@ -22,7 +22,7 @@ module pawn (
         leds0 = 7'b0000110; // E
       end
 
-      3'b011: begin
+      begin 3'b011: 
       leds5 = 7'b1000110; // C
       leds4 = 7'b0001001; // H
       leds3 = 7'b0000110; // E
@@ -30,7 +30,7 @@ module pawn (
       leds1 = 7'b0010010; // S
       end
 
-      3'b100: begin
+      begin 3'b100: 
       leds5 = 7'b1001000; // A
       leds4 = 7'b0001100; // P
       leds3 = 7'b0001100; // P
@@ -38,14 +38,14 @@ module pawn (
       leds1 = 7'b0000110; // E
       end
 
-       3'b101: begin
+      begin 3'b101: 
         leds4 = 7'b1000110; // C
         leds3 = 7'b1000000; // O
         leds2 = 7'b0001100; // P
         leds1 = 7'b0110001; // Y
       end
 
-      3'b110: begin
+      begin 3'b110:
         leds5 = 7'b1000110; // C
         leds4 = 7'b0001001; // H
         leds3 = 7'b0000110; // E
@@ -54,7 +54,14 @@ module pawn (
         leds0 = 7'b0000110; // E
       end
 
-      default: leds0 = 7'bX;
+      begin default: 
+      leds0 = 7'bX;
+      leds1 = 7'bX;
+      leds2 = 7'bX;
+      leds3 = 7'bX;
+      leds4 = 7'bX;
+      leds5 = 7'bX;
+      end
     endcase
   end
 endmodule  // pawn
