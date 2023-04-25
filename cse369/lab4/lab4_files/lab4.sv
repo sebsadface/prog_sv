4148ccad98e735e1cfa1cc51e928377df170d8da
Sebastian Liu <sebsadface@gmail.com>
2023-04-24 18:04:14 -0700

    ready for test

--- cse369/lab4/lab4_files/lab4.sv
+++ cse369/lab4/lab4_files/lab4.sv
@@ -4,16 +4,7 @@ module lab4 (
     output logic [6:0] HEX0, HEX1, HEX3, HEX4, HEX5
     );
 
-    logic U, P, C, M, DISCOUNTED, STOLEN;
-
-    assign U = SW[3];
-    assign P = SW[2];
-    assign C = SW[1];
-    assign M = SW[0];
-    assign DISCOUNTED = P | (C & U);
-    assign STOLEN = ~(U | M | C) | (U & ~(P | M));
-    assign LEDR[1] = DISCOUNTED;
-    assign LEDR[0] = STOLEN;
-    pawn SAFE (.upc(SW[3:1]), .leds0(HEX0), .leds1(HEX1), .leds2(HEX2), .leds3(HEX3), .leds4(HEX4), .leds5(HEX5));
+    lab3 STATUS (.SW(SW), .LEDR(LEDR));
+    pawn ITEMS (.upc(SW[3:1]), .leds0(HEX0), .leds1(HEX1), .leds2(HEX2), .leds3(HEX3), .leds4(HEX4), .leds5(HEX5));
 
 endmodule
