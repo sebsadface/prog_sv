6013f2d4697aa3edceb4d7587e1788593a5a047e
Sebastian Liu <sebsadface@gmail.com>
2023-04-30 22:26:47 -0700

    lab5 update

--- cse369/lab5/lab5_files/runwayLights.sv
+++ cse369/lab5/lab5_files/runwayLights.sv
@@ -4,8 +4,6 @@ module runwayLights (
 );
 
   // State Encoding and Variables
-  // An enum is a straightforward way to assign clear names to our FSM states
-  // while also being able to specify the bit representation of each state.
   // ps = "present state", ns = "next state".
   enum logic [2:0] {S101 = 3'b101, S001 = 3'b001, S010 = 3'b010, S100 = 3'b100} ps, ns;
   
@@ -31,16 +29,10 @@ module runwayLights (
         default:                    ns = ps;
     endcase
 
-    assign out = ns;
-
   // Output logic
-  // Note: this statement could be placed above the FSM case statement, in a
-  // separate always_comb block, or outside the always_comb block as an
-  // assign statement because it depends only on the current value of ps.
+  assign out = ns;
 
   // Sequential Logic
-  // Only state is FSM state
-  // Synchronous reset signal to set FSM to its first state
   always_ff @(posedge clk)
     if (reset)
       ps <= S101;
