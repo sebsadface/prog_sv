b9cc22ee0f67796df4d96094effbd459b2ff13f2
Sebastian Liu <sebsadface@gmail.com>
2023-05-06 22:47:29 -0700

    bug fix

--- cse369/lab6/lab6_files/tow.sv
+++ cse369/lab6/lab6_files/tow.sv
@@ -1,6 +1,6 @@
 module tow (
   input  logic       CLOCK_50,  // 50MHz clock
-  output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
+  output logic [6:0] HEX0,
   output logic [9:0] LEDR,
   input  logic [3:0] KEY,     // True when not pressed, False when pressed
   input  logic [9:0] SW
@@ -10,8 +10,8 @@ module tow (
     logic [1:0] winner;
 
     assign reset = SW[9];
-    user_input_handler player1 (.clk(CLOCK_50), .reset(reset), .in(~KEY[0]), .out(R));
-    user_input_handler player2 (.clk(CLOCK_50), .reset(reset), .in(~KEY[3]), .out(L));
+    user_input_handler player1 (.clk(CLOCK_50), .reset(reset), .in(KEY[0]), .out(R));
+    user_input_handler player2 (.clk(CLOCK_50), .reset(reset), .in(KEY[3]), .out(L));
 
     playfield pf (.clk(CLOCK_50), .reset(reset), .L(L), .R(R), .LEDR(LEDR[9:1]), .winner(winner));
 
