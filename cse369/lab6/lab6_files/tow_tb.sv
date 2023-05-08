55f7f71536964f3c3db88194349388dc0bec1a22
Sebastian Liu <sebsadface@gmail.com>
2023-05-07 19:42:53 -0700

    new testbenches

--- cse369/lab6/lab6_files/tow_tb.sv
+++ cse369/lab6/lab6_files/tow_tb.sv
@@ -1,19 +1,24 @@
  // Testbench for tow
-module tow_tb ();
-  logic clk;
+module tow_tb #(
+  parameter whichClock = 25
+  )
+  ();
+  logic [31:0] clk;
   logic [6:0] HEX0;
   logic [9:0] LEDR;
   logic [3:0] KEY;
   logic [9:0] SW;
 
-  tow dut (.CLOCK_50(clk), .HEX0, .LEDR, .KEY, .SW);
+clock_divider cdiv (.clock(CLOCK_50), .divided_clocks(clk));
+
+  tow dut (.CLOCK_50(clk[whichClock]), .HEX0, .LEDR, .KEY, .SW);
 
   // Set up the clock
-  parameter CLOCK_PERIOD=100;
-  initial begin
-    clk <= 0;
-    forever #(CLOCK_PERIOD*2) clk <= ~clk;
-  end
+//   parameter CLOCK_PERIOD=100;
+//   initial begin
+//     clk <= 0;
+//     forever #(CLOCK_PERIOD/2) clk <= ~clk;
+//   end
 
   // Set up the inputs to the design. Each line is a clock cycle.
   initial begin
