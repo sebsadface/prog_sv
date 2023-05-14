91481f52c8ee7364a0419d9f585378296b3e08a4
Sebastian Liu <sebsadface@gmail.com>
2023-05-14 15:28:34 -0700

    carryout bit bug  fix

--- cse369/lab7/lab7_files/adder1bit.sv
+++ cse369/lab7/lab7_files/adder1bit.sv
@@ -3,6 +3,6 @@ module adder1bit (
     output logic sum, cout
 );
     assign sum = a ^ b ^ cin;
-    assign cout = a & b + a & cin + b & cin;
+    assign cout = a & b | a & cin | b & cin;
 
 endmodule // adder1bit
