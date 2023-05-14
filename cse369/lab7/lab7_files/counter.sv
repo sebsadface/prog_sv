74aa9d7f90cd93ea1ef59c1e3081dbd316de355d
Sebastian Liu <sebsadface@gmail.com>
2023-05-14 16:26:47 -0700

    counter bug fix

--- cse369/lab7/lab7_files/counter.sv
+++ cse369/lab7/lab7_files/counter.sv
@@ -3,7 +3,7 @@ module counter (
     output logic [2:0] out
 );
 
-enum logic {S0 = 3'b000, S1 = 3'b001, S2 = 3'b010,  S3 = 3'b011, S4 = 3'b100, S5 = 3'b101, S6 = 3'b110, S7 = 3'b111} ps, ns;
+enum logic [2:0] {S0 = 3'b000, S1 = 3'b001, S2 = 3'b010,  S3 = 3'b011, S4 = 3'b100, S5 = 3'b101, S6 = 3'b110, S7 = 3'b111} ps, ns;
 
 always_comb
     case (ps)
