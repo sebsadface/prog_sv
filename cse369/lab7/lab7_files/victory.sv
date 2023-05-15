acd66c76dc8a11470a73bd41ce2d93ce2b59ee3f
Sebastian Liu <sebsadface@gmail.com>
2023-05-14 22:05:28 -0700

    victory bug fix

--- cse369/lab7/lab7_files/victory.sv
+++ cse369/lab7/lab7_files/victory.sv
@@ -7,17 +7,14 @@ module victory (
   logic [2:0] outL, outR;
   logic countL, countR;
 
-  always_ff @(posedge clk)
-    if (outL[0] & outL[1] & outL[2])
-      countR = 1'b0;
-    else
-      assign countR = (ledr1 & R);
+  always_ff @(posedge clk) begin
+    if (outL[0] & outL[1] & outL[2]) countR = 1'b0;
+    else                             countR = (ledr1 & R);
 
-    if (outR[0] & outR[1] & outR[2]) 
-      assign countL = 1'b0;
-    else
-      assign countL = (ledr9 & L);
-
+    if (outR[0] & outR[1] & outR[2]) countL = 1'b0;
+    else                             countL = (ledr9 & L);
+  end
+  
   counter left (.clk, .reset, .count(countL), .out(outL));
   counter right (.clk, .reset, .count(countR), .out(outR));
 
