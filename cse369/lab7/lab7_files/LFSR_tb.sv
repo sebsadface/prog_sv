a7ce7b9536e342460b633007d7ff59e3a4f62057
Sebastian Liu <sebsadface@gmail.com>
2023-05-15 14:19:33 -0700

    LFSR

--- cse369/lab7/lab7_files/LFSR_tb.sv
+++ cse369/lab7/lab7_files/LFSR_tb.sv
@@ -12,9 +12,9 @@ module LFSR_tb ();
     forever #(CLOCK_PERIOD/2) clk <= ~clk;
   end
    
+   assign Q = 9'b000000000;
    integer i;
   initial begin
-    Q <= 9'b000000000; @(posedge clk);
     for (i =0; i < 512; i++) begin
         @(posedge clk);
     end
