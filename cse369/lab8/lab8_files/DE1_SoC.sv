3f4bf6cab28898c58ed78fb3de3e7eaf265f9314
Sebastian Liu <sebsadface@gmail.com>
2023-05-25 15:54:52 -0700

    typo fix

--- cse369/lab8/lab8_files/DE1_SoC.sv
+++ cse369/lab8/lab8_files/DE1_SoC.sv
@@ -49,7 +49,7 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, SW, LEDR, GPIO_1, CLOCK
 		 =================================================================== */
 
 	user_input_handler next (.clk(SYSTEM_CLOCK), .reset(RST), .in(~KEY[0]), .out(select));
-	user_input_handler select (.clk(SYSTEM_CLOCK), .reset(RST), .in(~KEY[1], .out(next)));
+	user_input_handler select (.clk(SYSTEM_CLOCK), .reset(RST), .in(~KEY[1]), .out(next));
 	
 	playerSwitcher playerSwitcher (.clk(SYSTEM_CLOCK), .reset(RST), .select, .currentPlayer);
 	
