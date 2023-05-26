b1e2a084fba9f6ab30ac59cb50f23966473fd217
Sebastian Liu <sebsadface@gmail.com>
2023-05-25 19:48:42 -0700

    typo fix

--- cse369/lab8/lab8_files/emptyBoard.sv
+++ cse369/lab8/lab8_files/emptyBoard.sv
@@ -1,22 +1,22 @@
 module emptyBoard (
-    output [15:0][15:0] GrnPixels
+    output logic [15:0][15:0] GrnPixels
 );
 
-    GrnPixels[00] = 16'b1111111111111111;
-    GrnPixels[01] = 16'b1000010000100001;
-    GrnPixels[02] = 16'b1000010000100001;
-    GrnPixels[03] = 16'b1000010000100001;
-    GrnPixels[04] = 16'b1000010000100001;
-    GrnPixels[05] = 16'b1111111111111111;
-    GrnPixels[06] = 16'b1000010000100001;
-    GrnPixels[07] = 16'b1000010000100001;
-    GrnPixels[08] = 16'b1000010000100001;
-    GrnPixels[09] = 16'b1000010000100001;
-    GrnPixels[10] = 16'b1111111111111111;
-    GrnPixels[11] = 16'b1000010000100001;
-    GrnPixels[12] = 16'b1000010000100001;
-    GrnPixels[13] = 16'b1000010000100001;
-    GrnPixels[14] = 16'b1000010000100001;
-    GrnPixels[15] = 16'b1111111111111111;
+    assign GrnPixels[00] = 16'b1111111111111111;
+    assign GrnPixels[01] = 16'b1000010000100001;
+    assign GrnPixels[02] = 16'b1000010000100001;
+    assign GrnPixels[03] = 16'b1000010000100001;
+    assign GrnPixels[04] = 16'b1000010000100001;
+    assign GrnPixels[05] = 16'b1111111111111111;
+    assign GrnPixels[06] = 16'b1000010000100001;
+    assign GrnPixels[07] = 16'b1000010000100001;
+    assign GrnPixels[08] = 16'b1000010000100001;
+    assign GrnPixels[09] = 16'b1000010000100001;
+    assign GrnPixels[10] = 16'b1111111111111111;
+    assign GrnPixels[11] = 16'b1000010000100001;
+    assign GrnPixels[12] = 16'b1000010000100001;
+    assign GrnPixels[13] = 16'b1000010000100001;
+    assign GrnPixels[14] = 16'b1000010000100001;
+    assign GrnPixels[15] = 16'b1111111111111111;
 
 endmodule
